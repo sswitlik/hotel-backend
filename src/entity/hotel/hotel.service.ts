73c597945a7b91e73f363ce80d581b46a6a26949
sswitlik <szczepan.switlik@gmail.com>
2019-09-02 19:32:59 +0200

    getHotelsByRegion WithPurchases + pagination

--- src/entity/hotel/hotel.service.ts
+++ src/entity/hotel/hotel.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Hotel } from './hotel.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { activePurchaseStatuses } from '../purchase/_additionals/purchase-status.enum';
 
 @Injectable()
 export class HotelService extends TypeOrmCrudService<Hotel> {
@@ -12,6 +13,10 @@ export class HotelService extends TypeOrmCrudService<Hotel> {
   searchVacation(query: any) {
     const { from, to, pageSize, page } = query;
 
+    if (!pageSize || !page) {
+      throw new Error('Invalid pagination params');
+    }
+
     if (!query.regions) {
       throw new Error('Invalid regions parameter');
     }
@@ -19,6 +24,12 @@ export class HotelService extends TypeOrmCrudService<Hotel> {
 
     return this.repo.createQueryBuilder('hotel')
       .innerJoinAndSelect('hotel.region', 'region', 'region.name IN (:...regions)', { regions })
+      .leftJoinAndSelect('hotel.rooms', 'room')
+      .leftJoinAndSelect('room.purchases', 'purchase',
+        'purchase.status IN (:...statuses) AND purchase.termTo > :now_date',
+        { statuses: activePurchaseStatuses, now_date: new Date() })
+      .skip(pageSize * page)
+      .take(pageSize)
       .getMany();
   }
 }
